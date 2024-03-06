9d7e5c82d1f13631ba676c267942e0b2e4604278
Smooth Operator <github@andy.disruptek.com>
2024-03-06 15:35:17 -0500

    hide some ugly bits

--- pcp/registry.nim
+++ pcp/registry.nim
@@ -2,42 +2,75 @@ import std/genasts
 import std/macrocache
 import std/macros
 
-var fn: seq[pointer]
-fn.add(nil)
+type
+    Fun*[T, R] {.union.} = object
+      fn: Fn[T, R]
+      n: int
+      p: pointer
 
-macro register*(fun: typed): untyped =
-  genAstOpt({}, fun, fn=bindSym"fn", name=fun):
-    fn.add(cast[pointer](fun))
+    Fn*[T, R] = proc(x: ptr T; r: ptr R) {.nimcall.}
 
-proc ser(fun: NimNode): NimNode =
-  genAstOpt({}, fn=bindSym"fn", p=fun):
+proc `=copy`[T, R](target: var Fun[T, R]; source: Fun[T, R]) =
+  target.n = source.n
+
+var functions: seq[pointer]
+functions.add(nil)
+
+macro register*[T, R](function: Fn[T, R]): untyped =
+  genAstOpt({}, function, functions=bindSym"functions"):
+    functions.add(cast[pointer](function))
+
+proc ser(fun: NimNode; tipe: NimNode): NimNode =
+  genAstOpt({}, tipe, functions=bindSym"functions", p=fun):
     var i: pointer = nil
     block found:
-      for index, item in fn.pairs:
-        if item == p:
+      for index, item in functions.pairs:
+        if item == cast[pointer](p):
           i = cast[pointer](index)
           break found
-      i = p
-    cast[int](i)
+      i = cast[pointer](p)
+    cast[tipe](i)
 
 proc deser(index: NimNode; tipe: NimNode): NimNode =
-  genAstOpt({}, tipe, fn=bindSym"fn", i=index):
-    if i <= fn.high:
-      cast[tipe](fn[i])
+  genAstOpt({}, index, tipe, functions=bindSym"functions"):
+    let n = index
+    if n <= functions.high:
+      cast[tipe](functions[n])
     else:
-      cast[tipe](i)
+      cast[tipe](n)
 
-proc deser(index: NimNode): NimNode =
-  deser(index, bindSym"pointer")
+when false:
+  proc deser(index: NimNode): NimNode =
+    deser(index, bindSym"pointer")
 
-macro serialize*(fun: typed): untyped =
-  ser fun
+macro serialize*[T, R](fun: Fn[T, R]): untyped =
+  let t = getTypeInst fun
+  let a = t[0][1][1].last # ptr -> bracket
+  let b = t[0][2][1].last # ptr -> bracket
+  ser(fun, nnkBracketExpr.newTree(bindSym"Fun", a, b))
 
-macro deserialize*(index: typed; tipe: typedesc): untyped =
+macro deserialize*(index: int; tipe: typedesc): untyped =
   deser(index, tipe)
 
-macro call*(index: typed; tipe: typedesc; args: varargs[typed]): untyped =
+macro call*(index: int; tipe: typedesc; args: varargs[typed]): untyped =
   result = deser(index, tipe)
   result = newCall result
   for arg in args.items:
     result.add arg
+
+proc fp(fun: NimNode): NimNode =
+  let t = getTypeInst fun
+  result = deser(newDotExpr(fun, ident"n"),
+                 nnkBracketExpr.newTree(bindSym"Fn", t[1], t[2]))
+
+macro fp*[T, R](fun: Fun[T, R]): untyped =
+  ## recover a function pointer from `fun`
+  result = fp(fun)
+
+{.push experimental: "callOperator".}
+macro `()`*[T, R](fun: Fun[T, R]; args: varargs[typed]): untyped =
+  ## call `fun` with `args`
+  result = newCall(fun.fp)
+  for arg in args.items:
+    result.add arg
+{.pop.}
--- tests/tapi.nim
+++ tests/tapi.nim
@@ -6,15 +6,13 @@ when compileOption"stacktrace":
 
 proc main =
   type
-    Fn[T, R] = proc(x: ptr T; r: ptr R) {.nimcall.}
-
     Foo[T] {.byref.} = object
-      fn: int
+      fn: Fun[Foo[T], T]
       a: T
       b: T
 
     Bar[T, R] {.byref.} = object
-      fn: int
+      fn: Fun[Bar[T, R], R]
       t: T
 
   proc fib[T, R](bar: ptr Bar[T, R]; r: ptr R) {.nimcall.} =
@@ -23,11 +21,10 @@ proc main =
     else:
       var x = bar[]
       x.t = bar[].t-1
-      var fn1 = x.fn.deserialize(Fn[Bar[T, R], R])
-      fn1(addr x, r)
+      x.fn(addr x, r)
       bar[].t = bar[].t-2
-      var fn2 = bar.fn.deserialize(Fn[Bar[T, R], R])
-      mustTail fn2(bar, r)
+      var p = bar[].fn.fp
+      mustTail p(bar, r)
 
   proc fib[T](foo: ptr Foo[T]; r: ptr T) {.nimcall.} =
     if foo[].a < 2:
@@ -37,11 +34,10 @@ proc main =
       var x: Bar[T, T]
       x.t = foo[].a-1
       x.fn = serialize: Fn[Bar[T, T], T] fib[T, T]
-      var fn1 = x.fn.deserialize(Fn[Bar[T, T], T])
-      fn1(addr x, r)
+      x.fn(addr x, r)
       foo[].a = foo[].a-2
-      var fn2 = foo.fn.deserialize(Fn[Foo[T], T])
-      mustTail fn2(foo, r)
+      var p = foo[].fn.fp
+      mustTail p(foo, r)
 
   proc calc[T](a, b: T): T =
     var foo: Foo[T]
@@ -49,14 +45,14 @@ proc main =
     foo.b = b
     register(Fn[Foo[T], T] fib[T])
     foo.fn = serialize: Fn[Foo[T], T] fib[T]
-    foo.fn.call(Fn[Foo[T], T], addr foo, addr result)
+    foo.fn(addr foo, addr result)
 
   proc calc[T, R](t: T): R =
     var bar: Bar[T, R]
     bar.t = t
     register(Fn[Bar[T, R], R] fib[T, R])
     bar.fn = serialize fib[T, R]
-    bar.fn.call(Fn[Bar[T, R], R], addr bar, addr result)
+    bar.fn(addr bar, addr result)
 
   doAssert 102334155 == calc(40'i32, 0)
   doAssert 102334155 == calc[int64, uint](40)
